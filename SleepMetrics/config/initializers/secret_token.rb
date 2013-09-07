af43c39d19781f2e4daebcb1a0b4479ad385abde
Susan Greenberg <susangr@seas.upenn.edu>
2013-09-07 15:28:35 -0400

    new project

---
+++ SleepMetrics/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SleepMetrics::Application.config.secret_token = '657a1ab26fb5d41b5546cc18f66651c7eda11930e7db5fcdd14073f6fdacfb4de0a83f47680bdbaf544347a583ac733bb9922bff9c8a9b9f67a3d0ba95814301'
