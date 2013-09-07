f5ccaba72bf73bf395e701d093a6a41a83c4742b
Susan Greenberg <susangr@seas.upenn.edu>
2013-09-07 19:44:37 -0400

    added auth key etc to controller

--- app/controllers/send_text_controller_controller.rb
+++ app/controllers/send_text_controller_controller.rb
@@ -6,14 +6,14 @@ class SendTextControllerController < ApplicationController
     def send_text_message
       number_to_send_to = params[:number_to_send_to]
 
-      twilio_sid = "abc123"
-      twilio_token = "foobar"
-      twilio_phone_number = "6165555555"
+      twilio_sid = "AC89803551e8f08fdfe1b884728f51d142"
+      twilio_token = "e9a0d9fec0bbebe0ae5cf1065e53df77"
+      twilio_phone_number = "2158390033"
 
       @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
 
       @twilio_client.account.sms.messages.create(
-        :from => "+1#{twilio_phone_number}",
+        :from => "+12158390033",
         :to => number_to_send_to,
         :body => "This is a message to alert you that your laptop has ben moved enough to trigger the alarm"
       )
