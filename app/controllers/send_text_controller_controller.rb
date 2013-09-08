b73f2f91e3a2499a338468a49733ab3a984c20be
Susan Greenberg <susangr@seas.upenn.edu>
2013-09-08 02:13:05 -0400

    sound, text, everything

--- app/controllers/send_text_controller_controller.rb
+++ app/controllers/send_text_controller_controller.rb
@@ -1,24 +1,36 @@
 class SendTextControllerController < ApplicationController
-  
+
   def index
-    end
-
-    def send_text_message
-      number_to_send_to = params[:number_to_send_to]
-
-      twilio_sid = "AC89803551e8f08fdfe1b884728f51d142"
-      twilio_token = "e9a0d9fec0bbebe0ae5cf1065e53df77"
-      twilio_phone_number = "2158390033"
-
-      @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
-
-      @twilio_client.account.sms.messages.create(
-        :from => "+12158390033",
-        :to => number_to_send_to,
-        :body => "This is a message to alert you that your laptop has ben moved enough to trigger the alarm"
-      )
-    end
-    
-     helper :all
+  end
   
+  def main
+    session[:phone_number] = params[:phone]
+  end
+
+  def send_text_message
+    number_to_send_to = session[:phone_number]
+
+    twilio_sid = "AC89803551e8f08fdfe1b884728f51d142"
+    twilio_token = "e9a0d9fec0bbebe0ae5cf1065e53df77"
+    twilio_phone_number = "2158390033"
+
+    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+
+    @twilio_client.account.sms.messages.create(
+    :from => "+12158390033",
+    :to => number_to_send_to,
+    :body => "BEE DO BEE DO BEE DO! SOMEONE HAS TOUCHED YOUR COMPUTER!!!! THE THIEF IS NOW LISTENING TO MINIONS! DO NOT PANIC!"
+    )
+  end
+  
+  def reset
+    if params[:phone] == session[:phone_number]
+      redirect_to ""
+    else
+      redirect_to "/ring_alarm"
+    end
+  end
+
+  helper :all
+
 end
